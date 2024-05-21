3180e619d8fb795be946175588e8b4a86df34d9b
Nikita <nik.andreev.99@yandex.ru>
2024-05-21 16:09:13 +0300

    Добавлен вход

--- src/pages/SignIn.vue
+++ src/pages/SignIn.vue
@@ -1,1 +1,42 @@
-<template></template>
+<template>
+  <h1>Вход</h1>
+  <form @submit.prevent="onSubmit">
+    <p>
+      <input type="email" required placeholder="Email" v-model.trim="email" />
+    </p>
+    <p>
+      <input
+        type="password"
+        required
+        placeholder="Пароль"
+        v-model.trim="password" />
+    </p>
+    <p>
+      <button type="submit">Войти</button>
+    </p>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const email = ref("");
+const password = ref("");
+
+const auth = getAuth();
+
+const onSubmit = async () => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    );
+
+    console.log(userCredential);
+  } catch (error) {
+    console.log(error);
+  }
+};
+</script>
