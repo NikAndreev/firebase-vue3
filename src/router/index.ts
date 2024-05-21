13e79bb19ee08d6ee1191805855d7c72b3986758
Nikita <nik.andreev.99@yandex.ru>
2024-05-21 19:38:59 +0300

    Добавлена домашняя страница

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import SignUp from "../pages/SignUp.vue";
 import SignIn from "../pages/SignIn.vue";
+import Home from "../pages/Home.vue";
 import User from "../pages/User.vue";
 
 const routes = [
@@ -8,16 +9,25 @@ const routes = [
     path: "/sign-up",
     name: "signUp",
     component: SignUp,
+    meta: { requiresAuth: false },
   },
   {
     path: "/sign-in",
     name: "signIn",
     component: SignIn,
+    meta: { requiresAuth: false },
+  },
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+    meta: { requiresAuth: true },
   },
   {
     path: "/user",
     name: "user",
     component: User,
+    meta: { requiresAuth: true },
   },
 ];
 
