719e5dd35aa7efa337f78c2ddfd188a4d0d91e80
Nikita <nik.andreev.99@yandex.ru>
2024-05-21 15:44:33 +0300

    Добавлена регистрация

---
+++ src/firebase/index.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD8u7dS90Wpm43exgTNuTSzzMmtgHqJIME",
+  authDomain: "vue-authorization-cf9a3.firebaseapp.com",
+  projectId: "vue-authorization-cf9a3",
+  storageBucket: "vue-authorization-cf9a3.appspot.com",
+  messagingSenderId: "1002683187874",
+  appId: "1:1002683187874:web:4659490203ec3aa15b0dfb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/pages/SignUp.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>Регистрация</h1>
+  <form @submit.prevent="onSubmit">
+    <p>
+      <input type="email" required placeholder="Email" v-model.trim="email" />
+    </p>
+    <p>
+      <input
+        type="password"
+        required
+        placeholder="Пароль"
+        v-model.trim="password" />
+    </p>
+    <p>
+      <button type="submit">Зарегистрироваться</button>
+    </p>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const email = ref("");
+const password = ref("");
+
+const auth = getAuth();
+
+const onSubmit = async () => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    );
+
+    console.log(userCredential);
+  } catch (error) {
+    console.log(error);
+  }
+};
+</script>
